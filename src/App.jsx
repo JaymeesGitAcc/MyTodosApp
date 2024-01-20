2c92e41fa57c8ceeeae6e44b55c11c19fa403aa1
Andrew James <mygithubacc3198@gmail.com>
2024-01-20 20:23:06 +0530

    modified css

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     return (
         <>
             <TodosContext.Provider value={{ todos, activeCategory, dispatch }}>
-                <Container className="my-4 md:my-[100px] mx-auto w-[95%] max-w-[600px] min-h-[300px] shadow rounded-2xl pt-4 px-4 bg-slate-900">
+                <Container className="my-4 md:my-[100px] mx-auto w-[95%] duration-150 max-w-[600px] min-h-[300px] shadow rounded-2xl pt-4 px-4 bg-slate-900">
                     <h1 className="text-slate-300 text-2xl font-mono text-center mb-4">
                         My Todos
                     </h1>
